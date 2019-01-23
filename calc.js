d1b945c0ec65f36837da19413c997432784dd17d
Michael Huss <mhuss@ucvts.org>
2019-01-23 13:02:58 -0500

    Finished all functions, final tweaks needed

--- calc.js
+++ calc.js
@@ -1,53 +1,73 @@
+let answer;
+let expression = [];
+let calcAnswer;
+
 function pi(){
-  document.getElementById("answer").textContent +=Math.PI;
+  document.getElementById("answer").textContent += Math.PI;
+  expression.push(Math.PI);
 }
 
 function zero(){
-  document.getElementById("answer").textContent +="0";
+  document.getElementById("answer").textContent += "0";
+  expression.push("0");
 }
 
+
 function one(){
-  document.getElementById("answer").textContent +="1";
+  document.getElementById("answer").textContent += "1";
+  expression.push("1");
 }
 
 function two(){
-  document.getElementById("answer").textContent +="2";
+  document.getElementById("answer").textContent += "2";
+  expression.push("2");
 }
 
 function three(){
-  document.getElementById("answer").textContent +="3";
+  document.getElementById("answer").textContent += "3";
+  expression.push("3");
+
 }
 
 function four(){
-  document.getElementById("answer").textContent +="4";
+  document.getElementById("answer").textContent += "4";
+  expression.push("4");
 }
 
 function five(){
-  document.getElementById("answer").textContent +="5";
+  document.getElementById("answer").textContent += "5";
+  expression.push("5");
 }
 
 function six(){
-  document.getElementById("answer").textContent +="6";
+  document.getElementById("answer").textContent += "6";
+  expression.push("6");
 }
 
 function seven(){
-  document.getElementById("answer").textContent +="7";
+  document.getElementById("answer").textContent += "7";
+  expression.push("7");
 }
 
 function eight(){
-  document.getElementById("answer").textContent +="8";
+  document.getElementById("answer").textContent += "8";
+  expression.push("8");
 }
 
 function nine(){
   document.getElementById("answer").textContent += "9";
+  expression.push("9");
 }
 
 function allclear(){
-  document.getElementById("answer").innerHTML = "";
+  document.getElementById("answer").innerHTML = " ";
+  calcAnswer = "";
+  expression = [];
 }
 
 function decimal(){
   document.getElementById("answer").textContent += ".";
+  expression.push(".");
 }
 
 function negate(){
@@ -55,4 +75,36 @@ function negate(){
   answer = eval(answer);
   answer *=-1;
   document.getElementById("answer").innerHTML = answer;
+  expression.push("-");
+}
+
+function multiply(){
+  document.getElementById("answer").textContent += " * ";
+  expression.push("*");
+}
+
+function divide(){
+  document.getElementById("answer").textContent += " / ";
+  expression.push("/");
+}
+
+function add(){
+  document.getElementById("answer").textContent += " + ";
+  expression.push("+");
+}
+
+function subtract(){
+  document.getElementById("answer").textContent += " - ";
+  expression.push("-");
+}
+
+function percent(){
+  document.getElementById("answer");
+  expression.push("/", "1", "0", "0");
+  equal();
+}
+
+function equal(){
+calcAnswer = eval(expression.join(''))
+document.getElementById("answer").innerHTML = calcAnswer;
 }
