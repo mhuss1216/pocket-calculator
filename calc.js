1cd43207a217bc9b9a1063ec1f708e09d3647170
mhuss1216 <43789771+mhuss1216@users.noreply.github.com>
2019-01-23 19:37:34 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -104,6 +104,12 @@ function percent(){
   equal();
 }
 
+function numberWithCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
 function equal(){
 calcAnswer = eval(expression.join(''))
 document.getElementById("answer").innerHTML = calcAnswer;
